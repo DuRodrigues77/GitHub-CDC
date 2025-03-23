64f9a4d81be0fd6841c10a4a52345851c44de74d
Du Rodrigues <durodrigues0793@gmail.com>
2025-03-23 13:46:37 -0300

    .

---
+++ Aulas CDC JavaScript/Aula 02- Declarando-variaveis/declaringVariables.js
@@ -0,0 +1,40 @@
+//  let const
+
+const primeiroValor = 10
+const segundoValor = 20
+
+console.log("primeiroValor + segundoValor:", primeiroValor+segundoValor);
+
+const valorTotal = primeiroValor + segundoValor;
+
+
+
+console.log("valorTotal", valorTotal)
+
+
+
+let terceiroValor = 50
+console.log(terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValorFinal", terceiroValor);
+
+let comunidade = "Dev Completo"
+
+
+
+comunidade = "CDC";
+console.log(`O nome é, ${comunidade} `);
+
+// camelcase para declarar variaveis
+
+const autorDoCurso = "Beatriz Schwartz";
+
+
+// padrao snake case 
+const autora_do_curso = "Beatriz Schwartz";
+
+const dudu = oioioioi
+console.log(dudu)
+
+
---
+++ Aulas CDC JavaScript/Aula 03-Tipos-de-dados/dataTypes.js
@@ -0,0 +1,9 @@
+const nome = "Du";
+const idade = 31;
+let temCabeloCurto = true;
+console.log("Tem cabelo curto?" , temCabeloCurto);
+
+console.log("5 meses depois...")
+
+teemCabeloCurto = false;
+console.log("Tem cabelo curto?", temCabeloCurto);
