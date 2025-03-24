7309abad16e531b0dba4a54c4a04cfd57832ae7d
Du Rodrigues <durodrigues0793@gmail.com>
2025-03-23 21:26:50 -0300

    .

--- Aulas CDC JavaScript/Aula 03-Tipos-de-dados/dataTypes.js
+++ Aulas CDC JavaScript/Aula 03-Tipos-de-dados/dataTypes.js
@@ -13,11 +13,6 @@
 // BigInt
 
 
-
-
-
-
-
 const nome = "Du";
 const idade = 31;
 let temCabeloCurto = true;
@@ -25,5 +20,26 @@ console.log("Tem cabelo curto?" , temCabeloCurto);
 
 console.log("5 meses depois...")
 
-teemCabeloCurto = false;
+temCabeloCurto = false;
 console.log("Tem cabelo curto?", temCabeloCurto);
+
+let profissao; 
+console.log(profissao)
+
+profissao = "Du virou dev";
+console.log(profissao)
+
+let carro = "HB20";
+console.log("Qual é o carro do Du?", carro);
+
+console.log("1 ano depois...")
+
+carro = "AMG Mercedes";
+console.log("Qual é o carro novo do Du?", carro);
+
+console.log("2 anos depois...")
+
+
+carro = null;
+console.log(carro);
+
