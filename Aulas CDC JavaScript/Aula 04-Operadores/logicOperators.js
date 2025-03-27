01c69b8d4af8452cbda5d0f8125ff15954e81d2f
Du Rodrigues <durodrigues0793@gmail.com>
2025-03-26 21:48:01 -0300

    .

--- Aulas CDC JavaScript/Aula 04-Operadores/logicOperators.js
+++ Aulas CDC JavaScript/Aula 04-Operadores/logicOperators.js
@@ -1,10 +1,37 @@
 // AND / OR / NOT
 // E   / OU / NEGAÇÃO 
-// &&  /
+// &&  / || /  !
 console.log("Maria foi parada em uma blitz");
 
 
-const idadeMaria = 17;
-const mariaPossuiCNH = false; 
+const idadeMaria = 18;
+const mariaPossuiCNH = true;
 // AND
-console.log("Maria vai ser presa?", idadeMaria >= 18 && mariaPossuiCNH === false);
+
+console.log("Maria vai ser presa ? ", idadeMaria >= 18 && mariaPossuiCNH === false);
+
+console.log("---------------------")
+
+console.log("Maria foi para a balada e na entrada precisava mostrar o documento")
+
+const mariaPossuiRg = true;
+
+console.log("Maria levou seu RG?" , mariaPossuiRg);
+
+console.log("Maria é maior de idade?", idadeMaria >= 18);
+
+console.log("Maria conseguiu compravar que é maior de idade?", mariaPossuiCNH === true || mariaPossuiRg === true);
+
+console.log("Marias entrou na balada!!");
+
+console.log("-------------------------")
+
+// NOT
+
+
+const gostaDeBeber = true;
+
+
+const mariaGostaDeBeber = !gostaDeBeber;
+
+console.log("Maria gosta de bebr?", mariaGostaDeBeber);
