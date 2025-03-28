8d423a95cb13cff05c2f3609a0245fe7cc5a5684
Du Rodrigues <durodrigues0793@gmail.com>
2025-03-27 22:38:40 -0300

    .

--- Aulas CDC JavaScript/Aula 05-Condicionais/conditionals.js
+++ Aulas CDC JavaScript/Aula 05-Condicionais/conditionals.js
@@ -1,6 +1,6 @@
 
-const idadeMaria = 16;
-const maiorIdade = 18; 
+const idadeMaria = 18;
+const maiorIdade = 18;
 
 
 if (idadeMaria >= 18){
@@ -11,3 +11,39 @@ if (idadeMaria >= 18){
     console.log("Maria nao pode dirigir!");
     console.log(`falta ${anosFaltantes} anos para Maria poder dirigir`);
  }
+
+// operadores ternarios
+// 
+// idadeMaria >= 18 ? console.log("Maria pode dirigir")
+//     : console.log("Maria nao pode dirigir");
+
+
+console.log("-----------------------")
+
+// // 0 a 12 anos -> infância
+// 13 a 17 anos -> adolescência
+// 18 a 25 anos -> jovem adulto
+// 26 a 60 anos -> adulto
+// mais que 60 anos -> idoso
+
+const idadePessoa = 61;
+
+if (idadePessoa >= 0 && idadePessoa <= 12){
+    console.log("Infância");
+}
+
+else if (idadePessoa >= 13 && idadePessoa <= 17) {
+    console.log("adolecência");
+}
+
+else if (idadePessoa >= 18 && idadePessoa <= 25){
+    console.log("jovem adulto");
+}
+
+else if (idadePessoa >= 26 && idadePessoa <= 60){
+    console.log("adulto")
+}
+
+else {
+    console.log("idoso");
+}
