5b74180781a14d08e8f45b3700b2402690f21c95
Du Rodrigues <durodrigues0793@gmail.com>
2025-03-27 21:01:21 -0300

    .

--- Aulas CDC JavaScript/Aula 04-Operadores/logicOperators.js
+++ Aulas CDC JavaScript/Aula 04-Operadores/logicOperators.js
@@ -4,8 +4,8 @@
 console.log("Maria foi parada em uma blitz");
 
 
-const idadeMaria = 18;
-const mariaPossuiCNH = true;
+const idadeMaria = 17;
+const mariaPossuiCNH = false;
 // AND
 
 console.log("Maria vai ser presa ? ", idadeMaria >= 18 && mariaPossuiCNH === false);
